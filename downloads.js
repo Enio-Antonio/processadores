e135ac33da339504184a99456ceafd934140ec9f
Enio-Antonio <146883699+Enio-Antonio@users.noreply.github.com>
2024-01-23 00:51:56 -0300

    Add files via upload

---
+++ downloads.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function alertarRedesSocias() {
+    alert("Ainda não está disponível!");
+}
+
+function downloadFile(filename, url) {
+    let element = document.createElement('a');
+    element.setAttribute('href', url);
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+const instagram = document.getElementById("instagram");
+
+const twitter = document.getElementById("twitter");
+
+const intel = document.getElementById("intel");
+
+const amd = document.getElementById("amd");
+
+instagram.addEventListener("click", alertarRedesSocias);
+
+twitter.addEventListener("click", alertarRedesSocias);
+
+intel.addEventListener('click', () => {downloadFile('intel_i9.txt', './intel_i9.txt')});
+
+amd.addEventListener('click', () => {downloadFile('amd_r9', './amd_r9.txt')});
---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function alertarRedesSocias() {
+    alert("Ainda não está disponível!");
+}
+
+function alertarTrouxa() {
+     alert("Trouxa");
+}
+
+function downloadFile(filename, url) {
+    let element = document.createElement('a');
+    element.setAttribute('href', url);
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+const botaoDownload = document.getElementById("download");
+
+const instagram = document.getElementById("instagram");
+
+const twitter = document.getElementById("twitter");
+
+botaoDownload.addEventListener("click",() => {downloadFile('intel_i9.txt', './intel_i9.txt')});
+
+instagram.addEventListener("click", alertarRedesSocias);
+
+twitter.addEventListener("click", alertarRedesSocias);
